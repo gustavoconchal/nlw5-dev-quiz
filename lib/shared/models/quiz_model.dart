d7ae795d5f70034edf30626c97eb3a5918b2cf07
gustavoconchal <xuxa.rp@gmail.com>
2021-04-22 06:15:01 -0300

    Adicionada database local em json, quizzes.json e user.json. Implementado o HomeRepository para utilizar os dados obtidos dos arquivos .json.

--- lib/shared/models/quiz_model.dart
+++ lib/shared/models/quiz_model.dart
@@ -1,6 +1,26 @@
+import 'dart:convert';
+
 import 'package:dev_quiz/shared/models/question_model.dart';
 
 enum Level { easy, medium, hard, expert }
+
+extension LevelStringExt on String {
+  Level get parse => {
+        "easy": Level.easy,
+        "medium": Level.medium,
+        "hard": Level.hard,
+        "expert": Level.expert
+      }[this]!;
+}
+
+extension LevelExt on Level {
+  String get parse => {
+        Level.easy: "Level.easy",
+        Level.medium: "Level.medium",
+        Level.hard: "Level.hard",
+        Level.expert: "Level.expert"
+      }[this]!;
+}
 
 class QuizModel {
   final String title;
@@ -20,4 +40,30 @@ class QuizModel {
   double percentage() {
     return this.questionAnswered / this.questions.length;
   }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'questions': questions.map((x) => x.toMap()).toList(),
+      'questionAnswered': questionAnswered,
+      'imagem': imagem,
+      'level': level.parse,
+    };
+  }
+
+  factory QuizModel.fromMap(Map<String, dynamic> map) {
+    return QuizModel(
+      title: map['title'],
+      questions: List<QuestionModel>.from(
+          map['questions']?.map((x) => QuestionModel.fromMap(x))),
+      questionAnswered: map['questionAnswered'],
+      imagem: map['imagem'],
+      level: map['level'].toString().parse,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory QuizModel.fromJson(String source) =>
+      QuizModel.fromMap(json.decode(source));
 }
