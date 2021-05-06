3f61e5808cf3a575a974270ddefbccb91a29ff2d
gustavoconchal <xuxa.rp@gmail.com>
2021-05-06 01:03:29 -0300

    Implementado o acesso aos dados para QuizWidget e AnswerWidget

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,11 +1,21 @@
 import 'package:dev_quiz/challenge/widgets/answer/answer_widget.dart';
 import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:dev_quiz/shared/models/answer_model.dart';
 import 'package:dev_quiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
+class QuizWidget extends StatefulWidget {
   final QuestionModel question;
   const QuizWidget({Key? key, required this.question}) : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AnswerModel answer(int index) => widget.question.answers[index];
 
   @override
   Widget build(BuildContext context) {
@@ -14,15 +24,18 @@ class QuizWidget extends StatelessWidget {
         children: [
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Text(question.title, style: AppTextStyles.heading20),
+            child: Text(widget.question.title, style: AppTextStyles.heading20),
           ),
           SizedBox(height: 24),
-          ...question.answers
-              .map((e) => AnswerWidget(
-                    title: e.title,
-                    isRight: e.isRight,
-                  ))
-              .toList()
+          for (var i = 0; i < widget.question.answers.length; i++)
+            AnswerWidget(
+              answer: answer(i),
+              isSelected: indexSelected == i,
+              onTap: () {
+                indexSelected = i;
+                setState(() {});
+              },
+            )
         ],
       ),
     );
